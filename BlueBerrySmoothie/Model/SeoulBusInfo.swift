e830c6cc3380a12cf007098ce95bc3a6a083ff38
yeji <49176310+Yeggggi@users.noreply.github.com>
2025-01-14 01:40:45 +0900

    [CHORE/#2] 서울 API 변수 설명 주석 추가

--- BlueBerrySmoothie/Model/SeoulBusInfo.swift
+++ BlueBerrySmoothie/Model/SeoulBusInfo.swift
@@ -9,29 +9,29 @@ struct BusInfo: Identifiable {
     var id: String {
         vehId // 각 버스의 ID는 vehId로 식별합니다.
     }
-    var busType: String
-    var congetion: String
-    var dataTm: String
-    var fullSectDist: String
-    var gpsX: Double
-    var gpsY: Double
-    var isFullFlag: String
-    var islastyn: String
-    var isrunyn: String
-    var lastStTm: String
-    var lastStnId: String
-    var nextStId: String
-    var nextStTm: String
-    var plainNo: String
-    var posX: Double
-    var posY: Double
-    var rtDist: String
-    var sectDist: String
-    var sectOrd: String
-    var sectionId: String
-    var stopFlag: String
-    var trnstnid: String
-    var vehId: String
+    var busType: String // 차량유형 (0:일반버스, 1:저상)
+    var congetion: String // 차량내부 혼잡도 (0 : 없음, 3 : 여유, 4 : 보통, 5 : 혼잡, 6 : 매우혼잡)
+    var dataTm: String // 제공시간
+    var fullSectDist: String // 정류소간 거리
+    var gpsX: Double // 맵매칭X좌표 (WGS84)
+    var gpsY: Double // 맵매칭Y좌표 (WGS84)
+    var isFullFlag: String // 만차여부
+    var islastyn: String // 막차여부
+    var isrunyn: String // 해당차량 운행여부
+    var lastStTm: String // 종점도착소요시간
+    var lastStnId: String // 최종정류장 ID
+    var nextStId: String // 다음정류소아이디
+    var nextStTm: String // 다음정류소도착소요시간
+    var plainNo: String // 차량번호
+    var posX: Double // 맵매칭X좌표 (GRS80)
+    var posY: Double // 맵매칭Y좌표 (GRS80)
+    var rtDist: String // 노선옵셋거리
+    var sectDist: String // 구간옵셋거리
+    var sectOrd: String // 구간순번
+    var sectionId: String // 구간ID
+    var stopFlag: String // 정류소도착여부 (1:도착, 0:운행중)
+    var trnstnid: String // 회차지 정류소ID
+    var vehId: String // 버스ID
 }
 
 // BusInfoParser 클래스는 기존과 동일하게 사용합니다.
